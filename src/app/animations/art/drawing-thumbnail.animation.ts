6e6e16c2237c15d32e378852aaace14c7a06c7c8
Miguel Romeral <devromeral@gmail.com>
2024-08-28 13:55:10 +0200

    Changed drawing thumbnail animation

--- src/app/animations/art/drawing-thumbnail.animation.ts
+++ src/app/animations/art/drawing-thumbnail.animation.ts
@@ -9,31 +9,37 @@ import {
 
 export const drawingThumbnailAnimation = trigger('fadeInOut', [
   transition(':enter', [
-    style({ opacity: 0, transform: 'scale(0)' }),
-    animate('300ms', style({ opacity: 1, transform: 'scale(1)' })),
+    style({ opacity: 0, transform: 'rotate3d(2, 5, 0, 90deg)' }),
+    animate(
+      '300ms',
+      style({ opacity: 1, transform: 'rotate3d(0, 0, 0, 0deg)' })
+    ),
   ]),
-  transition(':leave', [
-    animate('300ms', style({ opacity: 0, transform: 'scale(0)' })),
-  ]),
+  // transition(':leave', [
+  //   animate(
+  //     '5000ms',
+  //     style({ opacity: 0, transform: 'transform: rotate3d(1, 1, 0, -360deg)' })
+  //   ),
+  // ]),
 ]);
 
 export const artGalleryAnimation = trigger('listAnimation', [
-  transition('* <=> *', [
-    query(
-      ':enter, :leave',
-      [style({ opacity: 0, position: 'absolute', width: '100%' })],
-      { optional: true }
-    ),
-    query(
-      ':enter',
-      [style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))],
-      { optional: true }
-    ),
-    query(
-      ':leave',
-      [style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))],
-      { optional: true }
-    ),
-    animateChild(),
-  ]),
+  // transition('* <=> *', [
+  //   query(
+  //     ':enter, :leave',
+  //     [style({ opacity: 0, position: 'relative', width: '100%' })],
+  //     { optional: true }
+  //   ),
+  //   query(
+  //     ':enter',
+  //     [style({ opacity: 0 }), animate('10000ms', style({ opacity: 1 }))],
+  //     { optional: true }
+  //   ),
+  //   query(
+  //     ':leave',
+  //     [style({ opacity: 1 }), animate('10000ms', style({ opacity: 0 }))],
+  //     { optional: true }
+  //   ),
+  //   animateChild(),
+  // ]),
 ]);
