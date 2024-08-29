af708a84ced11804fd1cdcdc26df3818a7e53e5e
Miguel Romeral <devromeral@gmail.com>
2024-08-29 12:06:51 +0200

    Language read from ExperienceProject component

--- src/app/components/about/experience-project/experience-project.component.ts
+++ src/app/components/about/experience-project/experience-project.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CustomTranslatePipe } from '@app/pipes/translate/customtranslate';
+import { LanguageService } from '@app/services/language/language.service';
 import { IExperience } from '@models/about/cv.model';
 import { LanguageComponent } from '@models/components/LanguageComponent';
 import { TranslateModule } from '@ngx-translate/core';
 import { formattedDateMini } from '@utils/customization/date-utils';
+import { sortTechnologyByLevel } from '@utils/sorting/sort-utils';
+import { settingLanguage } from 'config/settings/language.config';
 
 @Component({
   selector: 'app-experience-project',
@@ -13,18 +16,25 @@ import { formattedDateMini } from '@utils/customization/date-utils';
   templateUrl: './experience-project.component.html',
   styleUrl: './experience-project.component.scss',
 })
-export class ExperienceProjectComponent extends LanguageComponent {
+export class ExperienceProjectComponent
+  extends LanguageComponent
+  implements OnInit
+{
   @Input() project!: IExperience;
-  @Input() currentLanguage!: string;
+  currentLanguage: string = settingLanguage.defaultValue;
 
-  constructor() {
+  constructor(private languageService: LanguageService) {
     super('SCREENS.CV.EXPERIENCE');
   }
 
+  ngOnInit() {
+    this.languageService.currentLanguage$.subscribe(lang => {
+      this.currentLanguage = lang;
+    });
+  }
+
   sortedTechnology() {
-    return this.project.tecnology?.sort(
-      (a, b) => (b.level ?? 0) - (a.level ?? 0)
-    );
+    return this.project.tecnology?.sort(sortTechnologyByLevel);
   }
 
   formatDateMini(date: Date) {
--- src/utils/sorting/sort-utils.ts
+++ src/utils/sorting/sort-utils.ts
@@ -1,3 +1,4 @@
+import { IExperienceTecnology } from '@models/about/cv.model';
 import { Collection } from '@models/art/collection.model';
 import { DrawingCharacter } from '@models/art/drawing-character.model';
 import { DrawingProduct } from '@models/art/drawing-product.model';
@@ -29,4 +30,11 @@ export function sortByTextAscending(a: string, b: string): number {
 
 export function sortCollectionsByOrder(a: Collection, b: Collection): number {
   return b.order - a.order;
+}
+
+export function sortTechnologyByLevel(
+  a: IExperienceTecnology,
+  b: IExperienceTecnology
+) {
+  return (b.level ?? 0) - (a.level ?? 0);
 }
