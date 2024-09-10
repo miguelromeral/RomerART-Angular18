0689c9f256397be1c62577c1fcceae0e563e034f
Miguel Romeral <devromeral@gmail.com>
2024-09-10 13:32:58 +0200

    Added custom icon to reference image

--- src/app/components/shared/flip-card-image/flip-card-image.component.ts
+++ src/app/components/shared/flip-card-image/flip-card-image.component.ts
@@ -3,12 +3,18 @@ import { ZoomImageComponent } from '../zoom-image/zoom-image.component';
 import { LanguageComponent } from '@models/components/LanguageComponent';
 import { TranslateModule } from '@ngx-translate/core';
 import { CustomTranslatePipe } from '@app/pipes/translate/customtranslate';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-flip-card-image',
   standalone: true,
-  imports: [NgIf, ZoomImageComponent, TranslateModule, CustomTranslatePipe],
+  imports: [
+    NgIf,
+    NgClass,
+    ZoomImageComponent,
+    TranslateModule,
+    CustomTranslatePipe,
+  ],
   templateUrl: './flip-card-image.component.html',
   styleUrl: './flip-card-image.component.scss',
 })
@@ -29,6 +35,24 @@ export class FlipCardImageComponent extends LanguageComponent {
 
   constructor() {
     super('COMPONENTS.FLIP-CARD');
+  }
+
+  iconToShow(): string {
+    if (this.url) {
+      if (this.url.includes('instagram')) {
+        return 'bi-instagram';
+      }
+      if (this.url.includes('reddit')) {
+        return 'bi-reddit';
+      }
+      if (this.url.includes('twimg')) {
+        return 'bi-twitter-x';
+      }
+      if (this.url.includes('pinimg') || this.url.includes('pinterest')) {
+        return 'bi-pinterest';
+      }
+    }
+    return 'bi-image';
   }
 
   onImageNotFound() {
